2cfb98de4278badd4354770f63ed190d7f831b9d
Mikhail Semin <bifot@bifot.ru>
2018-09-06 22:45:45 +0300

    inital

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'newline-after-var': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-console': ['error', {
+      allow: ['error'],
+    }],
+  },
+  globals: {
+    before: true,
+    beforeEach: true,
+    after: true,
+    afterEach: true,
+    it: true,
+    describe: true,
+  },
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# @bifot/eslint-config
+
+Eslint config based on airbnb-base with a few needed rules.
+
+## Install
+
+```sh
+$ npm i @bifot/eslint-config -D
+```
+
+## Usage
+
+In your eslint config:
+
+```js
+{
+  extends: '@bifot/eslint-config',
+  ...
+}
+```
+
+## License
+
+MIT.
